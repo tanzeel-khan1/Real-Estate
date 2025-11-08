ef821561b924fe5ffcdd91754947e6043fd73a5c
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-11-08 11:55:40 +0500

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
 import React from 'react'
 import Nav from './component/Nav';
+import Hero from './pages/Hero';
 
 const page = () => {
   return (
     <div>
       <Nav/>
+     <div className='md:pl-24 pl-12 md:pr-16 pr-8'>
+       <Hero/>
+     </div>
     </div>
   )
 }
