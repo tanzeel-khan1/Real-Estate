ae1ac4798f06ed0a8925f8becc502cdaf32d9899
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-11-22 11:32:08 +0500

    r/a

--- app/page.tsx
+++ app/page.tsx
@@ -34,11 +34,10 @@
 import React from "react";
 import Nav from "./component/Nav";
 import Hero from "./pages/Hero";
-import Page from "./property/page";     // OK
+import Page from "./property/page";   
 import Center from "./pages/Center";
 import Cen from "./pages/Cen";
 import Proper from "./pages/Proper";
-import AboutPage from "./about/page";   // FIXED
 import Footer from "./pages/Footer";
 
 const page = () => {
@@ -55,7 +54,6 @@ const page = () => {
       <Cen />
       <Proper />
 
-      <AboutPage />  
       <Footer />
     </div>
   );
